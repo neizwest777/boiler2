116c83608c332365a03897f6a31999452a6fedca
vadim <prismestonia@gmail.com>
2025-11-13 17:20:23 +0200

    Update page.tsx

--- app/konsultatsioon/page.tsx
+++ app/konsultatsioon/page.tsx
@@ -1,7 +1,6 @@
 // @ts-nocheck
 "use client";
 
-import { useState } from "react";
 import Header from '@/components/shared/Header';
 import Footer from '@/components/shared/Footer';
 import { Button } from '@/components/shared/ui/button';
