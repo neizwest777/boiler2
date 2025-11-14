8d382fafca115c0d43259e7e84d209b360643b6f
vadim <prismestonia@gmail.com>
2025-11-14 09:22:16 +0200

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,22 +1,25 @@
-import { MetadataRoute } from 'next';
-import { allBlogs } from 'contentlayer/generated';
-import { siteConfig } from '@/data/config/site.settings';
+import { MetadataRoute } from "next";
+import { siteConfig } from "@/data/config/site.settings";
 
-export const dynamic = 'force-static';
+export const dynamic = "force-static";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const siteUrl = siteConfig.siteUrl;
-  const blogRoutes = allBlogs
-    .filter((post) => !post.draft)
-    .map((post) => ({
-      url: `${siteUrl}/${post.path}`,
-      lastModified: post.lastmod || post.date,
-    }));
 
-  const routes = ['', 'overview', 'tags'].map((route) => ({
+  const staticRoutes = [
+    "",
+    "meist",
+    "kontakt",
+    "paigaldus",
+    "remont",
+    "hooldus",
+    "hadabi",
+    "hinnad",
+    "garantii",
+  ].map((route) => ({
     url: `${siteUrl}/${route}`,
-    lastModified: new Date().toISOString().split('T')[0],
+    lastModified: new Date().toISOString().split("T")[0],
   }));
 
-  return [...routes, ...blogRoutes];
+  return staticRoutes;
 }
