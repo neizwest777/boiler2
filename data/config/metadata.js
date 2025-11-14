35b222740e92c001737469e359e263823b50eb8b
vadim <prismestonia@gmail.com>
2025-11-14 22:21:10 +0200

    Update metadata.js

--- data/config/metadata.js
+++ data/config/metadata.js
@@ -1,14 +1,28 @@
 const metadata = {
-  title: 'Boileri paigaldus, remont ja hooldus Tallinnas | Boileriabi.ee',
-  description: 'Boileriabi.ee – professionaalne boilerite paigaldus, remont ja hooldus Tallinnas ja Harjumaal. Kiire hädaabi 24/7, kogenud torumehed ja töögarantii. Tasuta konsultatsioon!',
+  title: 'Boileri Paigaldus, Remont ja Hooldus Tallinnas | Boileriabi.ee',
+  description: 'Kogenud torumees Tallinnas - boileri paigaldus, remont, hooldus ja 24/7 hädaabi. Tasuta hindamine, kiire reageerimine ja töögarantii. Boileriabi.ee - usaldusväline torumeesteenus Harjumaal.',
   domain: 'boileriabi.ee',
-  logoTitle: '',
+  logoTitle: 'Boileriabi.ee - Boileriteenused Tallinnas',
   businessName: 'Boileriabi.ee',
   siteUrl: 'https://boileriabi.ee',
   siteRepo: '',
   socialBanner: '/api/og',
   supportEmail: 'info@boileriabi.ee',
   email: 'info@boileriabi.ee',
+  telephone: '+37253684587',
+  address: {
+    streetAddress: '',
+    addressLocality: 'Tallinn',
+    addressRegion: 'Harjumaa',
+    postalCode: '',
+    addressCountry: 'EE'
+  },
+  openingHours: 'Mo-Su 00:00-23:59',
+  serviceAreas: ['Tallinn', 'Harjumaa', 'Harju maakond'],
+  geoLocation: {
+    latitude: '59.4370',
+    longitude: '24.7536'
+  },
   twitter: '',
   instagram: '',
   tiktok: '',
@@ -21,6 +35,17 @@ const metadata = {
   author: 'Boileriabi.ee',
   language: 'et',
   locale: 'et-EE',
+  keywords: [
+    'boileri paigaldus',
+    'boileri remont',
+    'boileri hooldus',
+    'torumees Tallinnas',
+    'hädaabi boiler',
+    'boileri teenused',
+    'boileri parandus',
+    'veesoojendi paigaldus',
+    '24/7 boileriabi'
+  ]
 };
 
 module.exports = { metadata };
