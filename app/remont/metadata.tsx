1bb8c5bbf111e761fb610230892d2c51bdb843c4
vadim <prismestonia@gmail.com>
2025-11-19 22:21:36 +0200

    Create metadata.tsx

---
+++ app/remont/metadata.tsx
@@ -0,0 +1,6 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Boileri Remont Tallinnas | Kiire Parandus | Boileriabi',
+  description: 'Boileri remont Tallinnas ja Harjumaal. Kiire reaktsioon, kvaliteetsed varuosad, garantii töödele. Hädaabi 24/7.',
+};
