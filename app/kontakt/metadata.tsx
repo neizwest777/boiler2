e8a9753c7de560e7e32838aa35e32b9c9c6e7630
vadim <prismestonia@gmail.com>
2025-11-19 22:20:11 +0200

    Create metadata.tsx

---
+++ app/kontakt/metadata.tsx
@@ -0,0 +1,6 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Kontakt - Boileriabi Tallinnas | Paigaldus, Remont, Hooldus',
+  description: 'Võta ühendust boileri paigalduse, remondi või hooldusega seotud küsimustes. Kiire vastus, tasuta konsultatsioon. Telefon: +37253684587',
+};
