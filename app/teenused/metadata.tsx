645702dcd7e2cf50682c0addb4b128a6fcb7ea95
vadim <prismestonia@gmail.com>
2025-11-19 22:07:56 +0200

    Create metadata.tsx

---
+++ app/teenused/metadata.tsx
@@ -0,0 +1,6 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Boileri Teenused | Paigaldus, Remont, Hooldus | Boileriabi',
+  description: 'Professionaalsed boileri teenused: paigaldus, remont, hooldus Tallinnas. Kiire reaktsioon, kvaliteetne töö ja garantiiaeg. Tasuta hindamine!',
+};
