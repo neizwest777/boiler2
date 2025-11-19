e358617f2634ea69a90d6652812f1fdc0b39f099
vadim <prismestonia@gmail.com>
2025-11-19 22:18:58 +0200

    Create metadata.tsx

---
+++ app/hinnad/metadata.tsx
@@ -0,0 +1,6 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Hinnad - Boileri Paigaldus, Remont ja Hooldus | Boileriabi',
+  description: 'Boileri paigalduse, remondi ja hoolduse hinnad Tallinnas. Tasuta hindamine, läbipaistvad hinnad, kvaliteetne töö.',
+};
