d61fdeb9c96807a349d3c195959a372154b782d0
vadim <prismestonia@gmail.com>
2025-11-19 22:22:32 +0200

    Create metadata.tsx

---
+++ app/terma/metadata.tsx
@@ -0,0 +1,6 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Kasutajatingimused - Boileriabi',
+  description: 'Boileriabi kasutajatingimused. Teave teenuse osutamise tingimuste kohta.',
+};
