d4dfac868b8e319b373fd19911e6372436be9aa9
vadim <prismestonia@gmail.com>
2025-11-19 22:18:29 +0200

    Create metadata.tsx

---
+++ app/hadaabi/metadata.tsx
@@ -0,0 +1,6 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Hädaabi 24/7 - Boileri Remont Tallinnas | Boileriabi',
+  description: 'Boileri hädaabi 24/7 Tallinnas. Kiire reageerimine lekete, rikete ja muude probleemide korral. Helista: +37253684587',
+};
