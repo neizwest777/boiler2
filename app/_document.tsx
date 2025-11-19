4515d243ce4618c35bf126624292208880ec7195
vadim <prismestonia@gmail.com>
2025-11-19 16:58:00 +0200

    Create _document.tsx

---
+++ app/_document.tsx
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="et">
+      <Head>
+        <script
+          src="https://analytics.ahrefs.com/analytics.js"
+          data-key="bomHtA+1BUw6NPo2b0TTrg"
+          async
+        ></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
