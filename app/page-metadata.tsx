0b8e9f8a5900e459e3411a55e7c89afedf3b1030
vadim <prismestonia@gmail.com>
2025-11-19 22:07:36 +0200

    Create page-metadata.tsx

---
+++ app/page-metadata.tsx
@@ -0,0 +1,6 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Boileri Paigaldus, Remont ja Hooldus Tallinnas | Boileriabi',
+  description: 'Kogenud torumehed boileri paigalduseks, remondiks ja hoolduseks Tallinnas. Kiire 24/7 hädaabi, töögarantii ja tasuta hindamine. Helista: +37253684587',
+};
