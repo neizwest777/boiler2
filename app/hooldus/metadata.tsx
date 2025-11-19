74d65318eee60d18f96cf564e658ed847849ad3b
vadim <prismestonia@gmail.com>
2025-11-19 22:19:35 +0200

    Create metadata.tsx

---
+++ app/hooldus/metadata.tsx
@@ -0,0 +1,6 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Boileri Hooldus Tallinnas | Puhastus ja Hooldus | Boileriabi',
+  description: 'Boileri hooldus ja puhastus Tallinnas. Katlakivi eemaldus, anoodi vahetus, süsteemi kontroll. Pikenda boileri eluiga!',
+};
