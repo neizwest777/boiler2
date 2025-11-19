6cdb6dfa9e2488393240352fbab7a6ac2581e6a1
vadim <prismestonia@gmail.com>
2025-11-19 22:19:53 +0200

    Create metadata.tsx

---
+++ app/konsultatsioon/metadata.tsx
@@ -0,0 +1,6 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Tasuta Konsultatsioon - Boileri Paigaldus ja Remont | Boileriabi',
+  description: 'Tasuta boileri konsultatsioon kogenud torumehe poolt. Soovitused paigalduseks, remondiks ja hoolduseks. Helista: +37253684587',
+};
