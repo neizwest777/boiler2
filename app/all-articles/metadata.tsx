22a1e77c445c7efd8baaeab1f58d9d8ab4026d1f
vadim <prismestonia@gmail.com>
2025-11-19 22:17:30 +0200

    Create metadata.tsx

---
+++ app/all-articles/metadata.tsx
@@ -0,0 +1,6 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Artiklid - Boileri Paigaldus ja Hooldus | Boileriabi',
+  description: 'Kasulikud artiklid boileri paigalduse, remondi ja hoolduse kohta. Nõuanded ja soovitused kogenud torumeestelt.',
+};
