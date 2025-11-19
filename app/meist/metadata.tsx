0b5ecc463ae7fabbab1531180875c6d135021ebb
vadim <prismestonia@gmail.com>
2025-11-19 22:20:27 +0200

    Create metadata.tsx

---
+++ app/meist/metadata.tsx
@@ -0,0 +1,6 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Meist - Boileriabi Tallinnas | Kogenud Torumehed',
+  description: 'Tutvu meie ettevõttega - kogenud torumehed boileri paigalduse, remondi ja hooldusega. Aastatepikkune kogemus, töögarantii.',
+};
