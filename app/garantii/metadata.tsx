5a81c8ad7de55a43f7df536692cfc6d09ec43588
vadim <prismestonia@gmail.com>
2025-11-19 22:18:07 +0200

    Create metadata.tsx

---
+++ app/garantii/metadata.tsx
@@ -0,0 +1,6 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Garantii - Boileri Paigaldus ja Remont | Boileriabi',
+  description: 'Boileri paigalduse ja remondi garantii tingimused. Kõikidele töödele anname garantii. Töökindel ja usaldusväärne teenus.',
+};
