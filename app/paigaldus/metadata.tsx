1c70043fe9057c01a2af5ab6f1b53ec886815603
vadim <prismestonia@gmail.com>
2025-11-19 22:20:50 +0200

    Create metadata.tsx

---
+++ app/paigaldus/metadata.tsx
@@ -0,0 +1,6 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Boileri Paigaldus Tallinnas | Professionaalne Teenus | Boileriabi',
+  description: 'Professionaalne boileri paigaldus Tallinnas. Tasuta hindamine, kiire paigaldus, töögarantii. Helista: +37253684587',
+};
