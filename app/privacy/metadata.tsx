0fcd6d30654e0ef33aa31905fe8514c073066ebd
vadim <prismestonia@gmail.com>
2025-11-19 22:21:10 +0200

    Create metadata.tsx

---
+++ app/privacy/metadata.tsx
@@ -0,0 +1,6 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Privaatsuspoliitika - Boileriabi',
+  description: 'Boileriabi privaatsuspoliitika. Teave isikuandmete kogumise ja kasutamise kohta.',
+};
