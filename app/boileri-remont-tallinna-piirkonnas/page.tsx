1cc9497779c229144f0e8298ca30e597799049e7
vadim <prismestonia@gmail.com>
2025-11-23 18:37:01 +0200

    Create page.tsx

---
+++ app/boileri-remont-tallinna-piirkonnas/page.tsx
@@ -0,0 +1,9 @@
+export default function BoileriRemontTallinnas() {
+  return (
+    <div>
+      <h1>Boileri Remont Tallinnas - Parandame Kiirelt</h1>
+      <p>Pakime boileri rikkeid kogu Tallinna piirkonnas. Tuleme kohale 2 tunni jooksul!</p>
+      {/* Аналогичная структура */}
+    </div>
+  )
+}
