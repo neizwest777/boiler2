2cb49e7e703812d7ef1e7e3c129fdef61d7ebdee
vadim <prismestonia@gmail.com>
2025-11-24 01:04:22 +0200

    Create route.ts

---
+++ app/robots.txt/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  const robots = `User-agent: *
+Allow: /
+
+Allow: /paigaldus
+Allow: /remont
+Allow: /hooldus
+Allow: /boileri-paigaldus-tallinna-piirkonnas
+Allow: /boileri-remont-tallinna-piirkonnas
+Allow: /boileri-hooldus-tallinna-piirkonnas
+Allow: /hadaabi
+Allow: /hinnad
+Allow: /meist
+Allow: /garantii
+Allow: /kontakt
+
+Disallow: /api/
+Disallow: /admin/
+Disallow: /private/
+
+Sitemap: https://www.boileriabi.ee/sitemap.xml`;
+
+  return new NextResponse(robots, {
+    status: 200,
+    headers: {
+      'Content-Type': 'text/plain',
+      'Cache-Control': 'public, max-age=86400',
+    },
+  });
+}
