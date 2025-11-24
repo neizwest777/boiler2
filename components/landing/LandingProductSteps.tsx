63aca8561a53492371d91557b5f05a0402e835e6
vadim <prismestonia@gmail.com>
2025-11-24 13:10:56 +0200

    Update LandingProductSteps.tsx

--- components/landing/LandingProductSteps.tsx
+++ components/landing/LandingProductSteps.tsx
@@ -1,12 +1,7 @@
-import { GlowBg } from '@/components/shared/ui/glow-bg';
-import clsx from 'clsx';
-import { Children } from 'react';
+import { GlowBg } from "@/components/shared/ui/glow-bg";
+import clsx from "clsx";
+import { Children } from "react";
 
-/**
- * A component meant to be used in the landing page.
- * It displays a title, description and a list of horizontal steps with a LandingProductFeature and/or LandingProductVideoFeature (in any combination, passed as children).
- * Use for more than 3 steps.
- */
 export const LandingProductSteps = ({
   className,
   children,
@@ -16,10 +11,10 @@ export const LandingProductSteps = ({
   descriptionComponent,
   withBackground = true,
   withBackgroundGlow = false,
-  variant = 'primary',
-  backgroundGlowVariant = 'primary',
-  containerType = 'wide',
-  display = 'list',
+  variant = "primary",
+  backgroundGlowVariant = "primary",
+  containerType = "wide",
+  display = "list",
 }: {
   className?: string;
   children?: React.ReactNode;
@@ -29,75 +24,68 @@ export const LandingProductSteps = ({
   descriptionComponent?: React.ReactNode;
   withBackground?: boolean;
   withBackgroundGlow?: boolean;
-  variant?: 'primary' | 'secondary';
-  backgroundGlowVariant?: 'primary' | 'secondary';
-  containerType?: 'narrow' | 'wide' | 'ultrawide';
-  display?: 'list' | 'grid';
+  variant?: "primary" | "secondary";
+  backgroundGlowVariant?: "primary" | "secondary";
+  containerType?: "narrow" | "wide" | "ultrawide";
+  display?: "list" | "grid";
 }) => {
   return (
     <section
       className={clsx(
-        'w-full flex justify-center items-center gap-8 p-6 py-12 lg:py-16 flex-col',
-        withBackground && variant === 'primary'
-          ? 'bg-primary-100/20 dark:bg-primary-900/10'
-          : '',
-        withBackground && variant === 'secondary'
-          ? 'bg-secondary-100/20 dark:bg-secondary-900/10'
-          : '',
-        withBackgroundGlow ? 'relative overflow-hidden' : '',
-        className,
+        "w-full flex flex-col justify-center items-center gap-10 p-6 py-12 lg:py-16",
+        withBackground &&
+          (variant === "primary"
+            ? "bg-primary-100/20 dark:bg-primary-900/10"
+            : "bg-secondary-100/20 dark:bg-secondary-900/10"),
+        withBackgroundGlow && "relative overflow-hidden",
+        className
       )}
     >
-      {withBackgroundGlow ? (
-        <div className="hidden lg:flex justify-center w-full h-full absolute -bottom-1/2 pointer-events-none">
+      {/* Background Glow */}
+      {withBackgroundGlow && (
+        <div className="hidden lg:flex justify-center w-full h-full absolute -bottom-1/3 pointer-events-none">
           <GlowBg
-            className={clsx('w-full lg:w-2/3 h-auto z-0')}
+            className="w-full lg:w-2/3 h-auto z-0"
             variant={backgroundGlowVariant}
           />
         </div>
-      ) : null}
+      )}
 
-      {title || description || titleComponent || descriptionComponent ? (
+      {/* TITLE + DESCRIPTION */}
+      {(title || description || titleComponent || descriptionComponent) && (
         <div
           className={clsx(
-            'relative w-full flex flex-col sm:items-center',
-            `container-${containerType}`,
+            "relative w-full flex flex-col sm:items-center",
+            `container-${containerType}`
           )}
         >
-          {titleComponent ||
-            (title && (
-              <h2 className="w-full text-2xl md:text-3xl lg:text-4xl font-semibold leading-tight md:leading-tight max-w-sm sm:max-w-none">
+          {titleComponent || (
+            title && (
+              <h2 className="w-full text-2xl md:text-3xl lg:text-4xl font-semibold leading-tight">
                 {title}
               </h2>
-            ))}
+            )
+          )}
 
           {descriptionComponent ||
             (description && (
               <p className="w-full mt-6 md:text-xl">{description}</p>
             ))}
         </div>
-      ) : null}
+      )}
 
+      {/* STEPS */}
       <div
         className={clsx(
-          '!p-0 w-full relative isolate gap-6 steps',
+          "relative w-full isolate",
           `container-${containerType}`,
-          variant,
-          display === 'list'
-            ? 'list flex flex-col'
-            : 'sgrid grid lg:grid-cols-3',
+          display === "list"
+            ? "flex flex-col gap-10"
+            : "grid gap-8 lg:grid-cols-3"
         )}
       >
-        {Children.map(children, (child, index) => (
-          <div
-            className={clsx(
-              'contents step',
-              display === 'list' && index % 2 === 1 ? 'odd' : '',
-              display === 'list' && index % 2 === 0 ? 'even' : '',
-            )}
-          >
-            {child}
-          </div>
+        {Children.map(children, (child) => (
+          <div className="w-full">{child}</div>
         ))}
       </div>
     </section>
