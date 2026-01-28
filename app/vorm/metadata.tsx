48088a97d59a3ec68739749cae9874d374fb6328
neizwest777 <prismestonia@gmail.com>
2026-01-28 13:28:05 +0200

    Add metadata for vorm page

---
+++ app/vorm/metadata.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Paringuvorm - Tasuta Hindamine | Boileriabi',
+  description: 'Saada paring boileri paigalduse, remondi voi hoolduse kohta. Tasuta hindamine ja kiire vastus. Taitke vorm ja votame teiega uhendust!',
+  keywords: 'boileri paring, tasuta hindamine, boileri tellimus, kontaktivorm',
+  openGraph: {
+    title: 'Paringuvorm - Tasuta Hindamine | Boileriabi',
+    description: 'Saada paring boileri teenuste kohta. Tasuta hindamine!',
+    url: 'https://www.boileriabi.ee/vorm',
+    siteName: 'Boileriabi',
+    locale: 'et_EE',
+    type: 'website',
+  },
+};
